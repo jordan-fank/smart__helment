916814e494b2dadc920627011a2801774dc00d22
jin yan <1426268894@qq.com>
2026-02-11 16:39:53 +0800

    Initial commit: Smart helmet project with MAX30102 heart rate/SpO2, MPU6050, GPS, smoke detection

--- MDK-ARM/smart_helmet/ringbuffer.d
+++
@@ -1,6 +0,0 @@
-smart_helmet\ringbuffer.o: ..\conponent\ringbuffer\ringbuffer.c
-smart_helmet\ringbuffer.o: ..\conponent\ringbuffer\ringbuffer.h
-smart_helmet\ringbuffer.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdint.h
-smart_helmet\ringbuffer.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdio.h
-smart_helmet\ringbuffer.o: D:\keil5\ARM\ARMCC\Bin\..\include\assert.h
-smart_helmet\ringbuffer.o: D:\keil5\ARM\ARMCC\Bin\..\include\string.h
